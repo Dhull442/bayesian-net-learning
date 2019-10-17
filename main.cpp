15bb516db8e498da8ae8f0b28ff375ab58801538
Saksham Dhull <sakshamdhull442@gmail.com>
2019-10-17 13:56:50 +0530

    processing record file

--- main.cpp
+++ main.cpp
@@ -99,216 +99,190 @@ public:
 
  // The whole network represted as a list of nodes
 class network{
-
+public:
 	list <Graph_Node> Pres_Graph;
 	string information;
-public:
-	int addNode(Graph_Node node)
-	{
+
+	int addNode(Graph_Node node){
 		Pres_Graph.push_back(node);
 		return 0;
 	}
 
-
-	int netSize()
-	{
+	int netSize(){
 		return Pres_Graph.size();
 	}
-    // get the index of node with a given name
-    int get_index(string val_name)
-    {
-        list<Graph_Node>::iterator listIt;
-        int count=0;
-        for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++)
-        {
-            if(listIt->get_name().compare(val_name)==0)
-                return count;
-            count++;
-        }
-        return -1;
+
+  // get the index of node with a given name
+  int get_index(string val_name){
+    list<Graph_Node>::iterator listIt;
+    int count=0;
+    for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++){
+        if(listIt->get_name().compare(val_name)==0)
+            return count;
+        count++;
     }
-// get the node at nth index
-    list<Graph_Node>::iterator get_nth_node(int n)
-    {
-       list<Graph_Node>::iterator listIt;
-        int count=0;
-        for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++)
-        {
-            if(count==n)
-                return listIt;
-            count++;
-        }
+    return -1;
+  }
+
+	// get the node at nth index
+  list<Graph_Node>::iterator get_nth_node(int n){
+	  list<Graph_Node>::iterator listIt;
+	  int count=0;
+	  for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++){
+	      if(count==n)
+	          return listIt;
+	      count++;
+	  }
+	  return listIt;
+  }
+
+  //get the iterator of a node with a given name
+  list<Graph_Node>::iterator search_node(string val_name){
+    list<Graph_Node>::iterator listIt;
+    for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++){
+      if(listIt->get_name().compare(val_name)==0)
         return listIt;
     }
-    //get the iterator of a node with a given name
-    list<Graph_Node>::iterator search_node(string val_name)
-    {
-        list<Graph_Node>::iterator listIt;
-        for(listIt=Pres_Graph.begin();listIt!=Pres_Graph.end();listIt++)
-        {
-            if(listIt->get_name().compare(val_name)==0)
-                return listIt;
-        }
+    cout<<"node not found\n";
+    return listIt;
+  }
 
-            cout<<"node not found\n";
-        return listIt;
-    }
+	void print(){
+		int i=0;
+		for (list<Graph_Node>::iterator it = Pres_Graph.begin(); it != Pres_Graph.end(); ++it){
+  		cout <<i<<" --> ";
+			it->print();
+			i++;
 
-		void print(){
-			int i=0;
-			for (list<Graph_Node>::iterator it = Pres_Graph.begin(); it != Pres_Graph.end(); ++it){
-    		cout <<i<<" --> ";
-				it->print();
-				i++;
+		}
+	}
 
+	void print_format(string filename){
+		ofstream file(filename);
+		time_t now = time(0);
+		char* dt = ctime(&now);
+		file << "// Bayesian Network in the Interchange Format\n// Output created "<< dt <<"// Bayesian network\n";
+		file << "network \"Alarm\" { //37 variables and 37 probability distributions\n}"<<endl;
+		// print variables
+		for (list<Graph_Node>::iterator it = Pres_Graph.begin(); it != Pres_Graph.end(); ++it){
+  		file << "variable  "<<it->Node_Name<<" { //"<<it->nvalues<<" values"<<endl;
+			file << "type discrete["<<it->nvalues<<"] { ";
+			for(int i=0;i<it->values.size();i++){
+				file << " " << it->values[i] << " ";
+			}
+			file << " };\n}\n";
+		}
+		// print values
+		for (list<Graph_Node>::iterator it = Pres_Graph.begin(); it != Pres_Graph.end(); ++it){
+			// file << "noth"<<endl;
+			int v = 1;
+			file << "probability (  "<<it->Node_Name<<" ";
+			for(int i=0;i<it->Parents.size();i++){
+				file<<" "<<it->Parents[i]<<" ";
+				v ++;
+			}
+			file <<" ) { //"<<v<<" variable(s) and"<<it->CPT.size()<<" values\n";
+			file <<"\ttable ";
+			for(int i=0;i<it->CPT.size();i++){
+				file << std::fixed << std::setprecision(4) << it->CPT[i]<<" ";
+			}
+			file<<";\n}\n";
+		}
+	}
+	void process(string filename){
+		ifstream dat(filename);
+		int i=0;
+		if(dat.is_open()){
+			while(!dat.eof()){
+				string line;
+				getline(dat,line);
+				vector< string > vals;
+				while(line.length()>0){
+					string k;
+					if(line.find(' ')<line.length()){
+						k=line.substr(0,line.find(' '));
+						line = line.substr(line.find(' ')+1);
+					}
+					else{
+						k = line;
+						line = "";
+					}
+					vals.push_back(k);
+				}
+				cout << i <<" -- " <<vals.size() <<endl;
 			}
 		}
-		void print_format(string filename){
-			ofstream file(filename);
-			time_t now = time(0);
-			char* dt = ctime(&now);
-			file << "// Bayesian Network in the Interchange Format\n// Output created "<< dt <<"// Bayesian network\n";
-			file << "network \"Alarm\" { //37 variables and 37 probability distributions\n}"<<endl;
-			// print variables
-			for (list<Graph_Node>::iterator it = Pres_Graph.begin(); it != Pres_Graph.end(); ++it){
-    		file << "variable  "<<it->Node_Name<<" { //"<<it->nvalues<<" values"<<endl;
-				file << "type discrete["<<it->nvalues<<"] { ";
-				for(int i=0;i<it->values.size();i++){
-					file << " " << it->values[i] << " ";
-				}
-				file << " };\n}\n";
-			}
-			// print values
-			for (list<Graph_Node>::iterator it = Pres_Graph.begin(); it != Pres_Graph.end(); ++it){
-				// file << "noth"<<endl;
-				int v = 1;
-				file << "probability (  "<<it->Node_Name<<" ";
-				for(int i=0;i<it->Parents.size();i++){
-					file<<" "<<it->Parents[i]<<" ";
-					v ++;
-				}
-				file <<" ) { //"<<v<<" variable(s) and"<<it->CPT.size()<<" values\n";
-				file <<"\ttable ";
-				for(int i=0;i<it->CPT.size();i++){
-					file << it->CPT[i]<<" ";
-				}
-				file<<";\n}\n";
-			}
-		}
-
+		dat.close();
+	}
 };
 
-network read_network()
-{
+network read_network(){
 	network Alarm;
 	string line;
 	int find=0;
-  	ifstream myfile("alarm.bif");
 	string info = "";
-  	string temp;
-  	string name;
-  	vector<string> values;
-    if (myfile.is_open())
-    {
-    	while (! myfile.eof() )
-    	{
-    		stringstream ss;
-      		getline (myfile,line);
-					// linenum ++;
-
-      		ss.str(line);
-     		ss>>temp;
-
-
-     		if(temp.compare("variable")==0)
-     		{
-     				ss>>name;
-     				getline (myfile,line);
-						// linenum++;
-						// lastnum = linenum;
-     				stringstream ss2;
-     				ss2.str(line);
-     				for(int i=0;i<4;i++)
-     				{
-
-     					ss2>>temp;
-
-
-     				}
-     				values.clear();
-     				while(temp.compare("};")!=0)
-     				{
-     					values.push_back(temp);
-
-     					ss2>>temp;
-    				}
-     				Graph_Node new_node(name,values.size(),values);
-     				int pos=Alarm.addNode(new_node);
-
-
-     		}
-     		else if(temp.compare("probability")==0)
-     		{
-
-     				ss>>temp;
-     				ss>>temp;
-
-                    list<Graph_Node>::iterator listIt;
-                    list<Graph_Node>::iterator listIt1;
-     				listIt=Alarm.search_node(temp);
-                    int index=Alarm.get_index(temp);
-                    ss>>temp;
-                    values.clear();
-     				while(temp.compare(")")!=0)
-     				{
-                        listIt1=Alarm.search_node(temp);
-                        listIt1->add_child(index);
-     					values.push_back(temp);
-
-     					ss>>temp;
-
-    				}
-                    listIt->set_Parents(values);
-    				getline (myfile,line);
-     				stringstream ss2;
-
-     				ss2.str(line);
-     				ss2>> temp;
-
-     				ss2>> temp;
-
-     				vector<float> curr_CPT;
-                    string::size_type sz;
-     				while(temp.compare(";")!=0)
-     				{
-
-     					curr_CPT.push_back(atof(temp.c_str()));
-
-     					ss2>>temp;
-
-
-
-    				}
-
-                    listIt->set_CPT(curr_CPT);
-
-
-     		}
-            else
-            {
-
-            }
-
-
-
-
-
-    	}
-    	if(find==1)
-    	myfile.close();
+	ifstream myfile("alarm.bif");
+	string temp;
+	string name;
+	vector<string> values;
+  if (myfile.is_open()){
+  	while (!myfile.eof()){
+  		stringstream ss;
+  		getline (myfile,line);
+  		ss.str(line);
+   		ss>>temp;
+   		if(temp.compare("variable")==0){
+ 				ss>>name;
+ 				getline (myfile,line);
+ 				stringstream ss2;
+ 				ss2.str(line);
+ 				for(int i=0;i<4;i++){
+ 					ss2>>temp;
+ 				}
+ 				values.clear();
+ 				while(temp.compare("};")!=0){
+ 					values.push_back(temp);
+ 					ss2>>temp;
+				}
+ 				Graph_Node new_node(name,values.size(),values);
+ 				int pos=Alarm.addNode(new_node);
+   		}
+   		else if(temp.compare("probability")==0){
+ 				ss>>temp;
+ 				ss>>temp;
+        list<Graph_Node>::iterator listIt;
+        list<Graph_Node>::iterator listIt1;
+ 				listIt=Alarm.search_node(temp);
+        int index=Alarm.get_index(temp);
+        ss>>temp;
+        values.clear();
+ 				while(temp.compare(")")!=0)
+ 				{
+          listIt1=Alarm.search_node(temp);
+          listIt1->add_child(index);
+ 					values.push_back(temp);
+ 					ss>>temp;
+				}
+        listIt->set_Parents(values);
+				getline (myfile,line);
+ 				stringstream ss2;
+ 				ss2.str(line);
+ 				ss2>> temp;
+ 				ss2>> temp;
+ 				vector<float> curr_CPT;
+        string::size_type sz;
+ 				while(temp.compare(";")!=0)
+ 				{
+ 					curr_CPT.push_back(atof(temp.c_str()));
+ 					ss2>>temp;
+				}
+        listIt->set_CPT(curr_CPT);
+   		}
   	}
-
-  	return Alarm;
+  	if(find==1)
+  		myfile.close();
+	}
+	return Alarm;
 }
 
 int main()
@@ -320,4 +294,5 @@ int main()
 	// cout<<"Perfect! Hurrah! \n";
 	Alarm.print_format("output.bif");
 	// Alarm.print();
+	// Alarm.process("sample.dat");
 }
